fcc7c39db2bf8707bece8b9c3ce1130aa58fea04
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-04-23 14:03:16 -0600

    primer commit

---
+++ fundamentos_python.py
@@ -0,0 +1,82 @@
+print("----------")
+
+# suma
+a = 5
+b = 3
+print(a + b)
+
+# resta
+a = 5
+b = 3
+print(a - b)
+
+# multiplicación
+a = 5
+b = 3
+print(a * b)
+
+# división
+a = 5
+b = 3
+print(a / b)
+
+# división entera
+a = 5
+b = 3
+print(a // b)
+
+# resto de la división
+a = 5
+b = 3
+print(a % b)
+
+# potencia
+a = 5
+b = 3
+print(a ** b)
+
+# Declarar una variable y asignarle un valor
+x = 10
+print(x)
+
+# Declarar varias variables y asignarles valores
+y, z = 20, 30
+print(y)
+print(z)
+
+# Cambiar el valor de una variable
+x = 5
+print(x)
+# Números enteros (integers)
+x = 5
+y = -3
+z = 0
+print(x)
+print(y)
+print(z)
+
+# Números de punto flotante (floats)
+a = 3.14
+b = -2.5
+c = 0.0
+print(a)
+print(b)
+print(c)
+
+# Números complejos (complex numbers)
+d = 2 + 3j
+e = -1j
+print(d)
+print(e)
+
+# Cadenas de texto (strings)
+f = "Hola, mundo!"
+g = 'Python es genial'
+print(f)
+print(g)
+
+# Booleanos (boolean values)
+h = True
+i = False
+print(h)
+print(i)
